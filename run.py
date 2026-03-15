23a8a7f934f77216d0e2f9f43ac36082f3615e12
Matthew Meredith <merematt@udel.edu>
2026-03-15 17:55:51 -0400

    refactor. require solvers to be selected for every run

--- run.py
+++ run.py
@@ -8,73 +8,28 @@ from game import Game
 from informed import AStarSearch, IDAStarSearch
 from replay import Replay
 from solver import Solver
-from uninformed import BreadthFirstSearch
+from uninformed import BreadthFirstSearch, GamblersSearch
 
-default_states = ["problem1", "problem2", "problem3"]
+all_states = sorted(
+    [
+        os.path.basename(file).split(".")[0]
+        for file in os.listdir("states")
+        if os.path.isfile(os.path.join("states", file))
+    ]
+)
 all_solvers = {
+    "gamblers": GamblersSearch,
     "bfs": BreadthFirstSearch,
     "idastar": IDAStarSearch,
     "astar": AStarSearch,
 }
 
 
-def solve(
-    solvers: list[Solver], game: Game, save_solution: bool, max_iterations: int | None
-) -> None:
-    state = game.snapshot()
-    for solver in solvers:
-        solver.solve(game, save_solution=save_solution, max_iterations=max_iterations)
-        game.restore_snapshot(state)
-        print("\n")
-
-
-def solve_random(
-    solvers: list[Solver], size: int, save_solution: bool, max_iterations: int | None
-) -> None:
-    solve(
-        solvers,
-        Game.random_state(size),
-        save_solution=save_solution,
-        max_iterations=max_iterations,
-    )
-
-
-def replay_solutions(
-    solvers: list[Solver],
-    states: list[str],
-    save_solution: bool,
-    max_iterations: int | None,
-):
-    for solver in solvers:
-        solver_name = solver.get_name()
-        solution_dir = os.path.join("solutions", solver_name)
-        all_solutions = [
-            solution
-            for solution in os.listdir(solution_dir)
-            if os.path.isfile(os.path.join(solution_dir, solution))
-        ]
-        for state in states:
-            solutions = [
-                solution for solution in all_solutions if solution.startswith(state)
-            ]
-            if len(solutions) == 0:
-                cprint(f"No solutions found for state {solver_name}/{state}", "red")
-                continue
-            for solution_name in solutions:
-                solve(
-                    [Replay(solver_name=solver_name, solution_name=solution_name)],
-                    Game.from_state_file(state),
-                    save_solution=save_solution,
-                    max_iterations=max_iterations,
-                )
-
-
-if __name__ == "__main__":
+def main():
     parser = ArgumentParser()
     parser.add_argument(
-        "--solvers",
+        "solvers",
         nargs="+",
-        default=all_solvers.keys(),
         choices=all_solvers.keys(),
         help="List of solvers to use",
     )
@@ -86,7 +41,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--states",
         nargs="+",
-        default=default_states,
+        default=all_states,
         help="List of names (without extension) of problem states to solve (located in states/)",
     )
     parser.add_argument(
@@ -160,3 +115,58 @@ if __name__ == "__main__":
                 save_solution=args.save_solution,
                 max_iterations=args.max_iterations,
             )
+
+
+def solve(
+    solvers: list[Solver], game: Game, save_solution: bool, max_iterations: int | None
+) -> None:
+    state = game.snapshot()
+    for solver in solvers:
+        solver.solve(game, save_solution=save_solution, max_iterations=max_iterations)
+        game.restore_snapshot(state)
+        print("\n")
+
+
+def solve_random(
+    solvers: list[Solver], size: int, save_solution: bool, max_iterations: int | None
+) -> None:
+    solve(
+        solvers,
+        Game.random_state(size),
+        save_solution=save_solution,
+        max_iterations=max_iterations,
+    )
+
+
+def replay_solutions(
+    solvers: list[Solver],
+    states: list[str],
+    save_solution: bool,
+    max_iterations: int | None,
+):
+    for solver in solvers:
+        solver_name = solver.get_name()
+        solution_dir = os.path.join("solutions", solver_name)
+        all_solutions = [
+            solution
+            for solution in os.listdir(solution_dir)
+            if os.path.isfile(os.path.join(solution_dir, solution))
+        ]
+        for state in states:
+            solutions = [
+                solution for solution in all_solutions if solution.startswith(state)
+            ]
+            if len(solutions) == 0:
+                cprint(f"No solutions found for state {solver_name}/{state}", "red")
+                continue
+            for solution_name in solutions:
+                solve(
+                    [Replay(solver_name=solver_name, solution_name=solution_name)],
+                    Game.from_state_file(state),
+                    save_solution=save_solution,
+                    max_iterations=max_iterations,
+                )
+
+
+if __name__ == "__main__":
+    main()
