4bfdc0068db233e09274c8248e8ac75007a17258
Matthew Meredith <merematt@udel.edu>
2026-03-15 16:29:09 -0400

    reactor runner to be more organized and support replays

--- run.py
+++ run.py
@@ -2,73 +2,152 @@ import os
 from argparse import ArgumentParser
 from shutil import rmtree
 
+from termcolor import cprint
+
 from game import Game
 from informed import AStarSearch, IDAStarSearch
+from replay import Replay
+from solver import Solver
 from uninformed import BreadthFirstSearch
 
-states = ["problem1", "problem2", "problem3"]
-solvers = {
+default_states = ["problem1", "problem2", "problem3"]
+all_solvers = {
     "bfs": BreadthFirstSearch,
     "idastar": IDAStarSearch,
     "astar": AStarSearch,
 }
 
 
+def solve(
+    solvers: list[Solver], game: Game, save_solution: bool, max_iterations: int | None
+) -> None:
+    state = game.snapshot()
+    for solver in solvers:
+        solver.solve(game, save_solution=save_solution, max_iterations=max_iterations)
+        game.restore_snapshot(state)
+        print("\n")
+
+
+def solve_random(
+    solvers: list[Solver], size: int, save_solution: bool, max_iterations: int | None
+) -> None:
+    solve(
+        solvers,
+        Game.random_state(size),
+        save_solution=save_solution,
+        max_iterations=max_iterations,
+    )
+
+
+def replay_solutions(
+    solvers: list[Solver],
+    states: list[str],
+    save_solution: bool,
+    max_iterations: int | None,
+):
+    for solver in solvers:
+        solver_name = solver.get_name()
+        solution_dir = os.path.join("solutions", solver_name)
+        all_solutions = [
+            solution
+            for solution in os.listdir(solution_dir)
+            if os.path.isfile(os.path.join(solution_dir, solution))
+        ]
+        for state in states:
+            solutions = [
+                solution for solution in all_solutions if solution.startswith(state)
+            ]
+            if len(solutions) == 0:
+                cprint(f"No solutions found for state {solver_name}/{state}", "red")
+                continue
+            for solution_name in solutions:
+                solve(
+                    [Replay(solver_name=solver_name, solution_name=solution_name)],
+                    Game.from_state_file(state),
+                    save_solution=save_solution,
+                    max_iterations=max_iterations,
+                )
+
+
 if __name__ == "__main__":
-    fn = ArgumentParser()
-    fn.add_argument(
+    parser = ArgumentParser()
+    parser.add_argument(
         "--clean",
         action="store_true",
         help="Remove all old solution files before solving",
     )
-    fn.add_argument(
-        "--states", nargs="+", default=states, help="List of problem states to solve"
+    parser.add_argument(
+        "--random", type=int, help="Solve a random state with RANDOM bolts"
     )
-    fn.add_argument(
-        "--solvers", nargs="+", default=solvers.keys(), help="List of solvers to use"
+    parser.add_argument("--replay", action="store_true", help="Replay solutions")
+    parser.add_argument(
+        "--states",
+        nargs="+",
+        default=default_states,
+        help="List of problem states to solve",
     )
-    fn.add_argument("--random", type=int, help="Solve a random state with RANDOM bolts")
-    fn.add_argument(
+    parser.add_argument(
+        "--solvers",
+        nargs="+",
+        default=all_solvers.keys(),
+        help="List of solvers to use",
+    )
+    parser.add_argument(
         "--print-state", action="store_true", help="Print the state after each step"
     )
-    fn.add_argument(
+    parser.add_argument(
         "--step-delay-ms",
         type=int,
         default=0,
         help="Delay between steps in milliseconds",
     )
-    fn.add_argument(
+    parser.add_argument(
+        "--max-iterations",
+        type=int,
+        default=None,
+        help="Maximum number of iterations to run",
+    )
+    parser.add_argument(
         "--no-save",
         action="store_false",
         dest="save_solution",
         help="Do not save the solution",
     )
 
-    args = fn.parse_args()
+    args = parser.parse_args()
+
+    solver_instances = [
+        all_solvers[solver_name](
+            print_steps=args.print_state, step_delay_ms=args.step_delay_ms
+        )
+        for solver_name in args.solvers
+    ]
 
     if args.clean:
-        for solver_name in args.solvers:
-            solver = solvers[solver_name]()
+        for solver in solver_instances:
             rmtree(os.path.join("solutions", solver.get_name()), ignore_errors=True)
-        exit()
 
-    game = None
     if args.random:
-        game = Game.random_state(args.random)
-        args.states = [game.snapshot()]
-
-    for state in args.states:
-        for solver_name in args.solvers:
-            solver = solvers[solver_name](
-                print_steps=args.print_state, step_delay_ms=args.step_delay_ms
+        if args.replay:
+            raise RuntimeError("Cannot replay a random game")
+        solve_random(
+            solver_instances,
+            args.random,
+            save_solution=args.save_solution,
+            max_iterations=args.max_iterations,
+        )
+    elif args.replay:
+        replay_solutions(
+            solver_instances,
+            args.states,
+            save_solution=args.save_solution,
+            max_iterations=args.max_iterations,
+        )
+    else:
+        for state in args.states:
+            solve(
+                solver_instances,
+                Game.from_state_file(state),
+                save_solution=args.save_solution,
+                max_iterations=args.max_iterations,
             )
-            if args.clean:
-                rmtree(os.path.join("solutions", solver.get_name()), ignore_errors=True)
-            if isinstance(state, str):
-                game = Game.from_state_file(state)
-            elif game:
-                game.restore_snapshot(state)
-            else:
-                raise ValueError("Game not initialized")
-            solver.solve(game, save_solution=args.save_solution)
-            print("\n")
